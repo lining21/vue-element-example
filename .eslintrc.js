1bd9b8fe474e0d85a6bb91251642703ff0a0a187
lining <lining@qdcares.cn>
2020-11-24 13:29:42 +0800

    提交文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,41 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    '@vue/airbnb'
   ],
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'off',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
+    "global-require": 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-destructuring': 0,
+    'no-restricted-syntax': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    'no-unused-vars': 0,
+    complexity: ['error', 10],
+    'max-depth': ['error', 3],
+    // 引入
+    'import/prefer-default-export': 0,
+    // 行最大长度限制
+    'max-len': 0,
+    "template-curly-spacing" : "off",
+    "indent": ["error", 2],
+    "linebreak-style": [0 ,"error", "windows"]
+  },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}
+};
---
+++ vue.config.js
@@ -0,0 +1,91 @@
+const pxtorem = require('postcss-pxtorem');
+const webpack = require('webpack');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
+module.exports = {
+  publicPath: './',
+  lintOnSave: true,
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          pxtorem({
+            rootValue: 16,
+            propList: ['font', 'font-size']
+          })
+        ]
+      },
+      sass: {
+        prependData: '@import \'~@/style/utils.scss\';'
+      }
+    }
+  },
+  configureWebpack: {
+    // 优化打包配置
+    devtool: process.env.NODE_ENV === 'production' ? 'nosources-source-map' : 'eval-source-map',
+    plugins: [
+      new GitRevisionPlugin(),
+      new SpeedMeasurePlugin(),
+      new webpack.ContextReplacementPlugin(
+        /moment[/\\]locale$/,
+        /zh-cn/,
+      )
+
+    ]
+  },
+  chainWebpack: (config) => {
+    const vueRule = config.module.rule('vue').test(/\.vue$/);
+    vueRule.uses.clear();
+    vueRule.use('thread-loader')
+      .loader('thread-loader')
+      .end()
+      .use('cache-loader')
+      .loader('cache-loader')
+      .end()
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end();
+  },
+  // productionSourceMap: false, // 打包时去除map文件
+  devServer: {
+    headers: {
+      'X-Frame-Options': 'sameorigin'
+    },
+    open: true,
+    proxy: {
+      // '/amos/afpcflow/tc': {
+      //   target: process.env.VUE_APP_PROXY_URL2,
+      //   changeOrigin: true,
+      //   ws: true,
+      //   pathRewrite: {
+      //     '/amos/afpcflow/tc': '/tc'
+      //   }
+      // },
+      '/amos': {
+        target: process.env.VUE_APP_PROXY_URL,
+        changeOrigin: true,
+        ws: true
+        // pathRewrite: {
+        //   '^/amos': '',
+        // },
+      },
+      '/amos/sfss/space/ueditor': {
+        target: `${process.env.VUE_APP_PROXY_URL}/amos/sfss/space/ueditor`,
+        changeOrigin: true,
+        ws: true
+        // pathRewrite: {
+        //   '^/amos/workflow': '/amos/workflow-pc',
+        // },
+      },
+      '/ns_train': {
+        target: 'http://192.168.163.157:8000',
+        changeOrigin: true,
+        ws: true
+        // pathRewrite: {
+        //   '^/amos': '',
+        // },
+      }
+    }
+  }
+};
